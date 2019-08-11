e44bae48b251d5eb77340a1c2b34ebc428f1922c
Quang Vo <thanhquang2608@gmail.com>
2019-08-11 17:26:17 +0700

    Update script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,5 +3,7 @@
 # Example: Change bundle name of an iOS app for non-production
 if [ "$APPCENTER_BRANCH" != "master" ];
 then
-    plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/HelloAppcenter/Info.plist
+    plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/ios/HelloAppcenter/Info.plist
 fi
+
+echo "Changing ios bundle identifier" plutil -replace CFBundleIdentifier -string "com.smartapptech.helloappcenter" $APPCENTER_SOURCE_DIRECTORY/ios/HelloAppcenter/Info.plist
